722fba4dc17d9c46b75fed0ea210a041e1d2e797
michael bychkowski <mbychkowski@gmail.com>
2018-03-05 22:09:59 -0500

    setup configuration files (connection, orm)

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Krabby Patty", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The King Kong", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Big SQL", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Moo Moo", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Good Burger", true);
