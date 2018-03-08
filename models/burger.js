f74a9a45073476991070d8a78c0430513ace025e
michael bychkowski <mbychkowski@gmail.com>
2018-03-07 23:31:12 -0500

    update model parameters for routes

--- models/burger.js
+++ models/burger.js
@@ -3,20 +3,20 @@ var orm = require('../config/orm.js');
 var burger = {
 
   all: function(cb) {
-    orm.selectAll('burgers', function(res) {
-      cb(res);
+    orm.selectAll('burgers', function(result) {
+      cb(result);
     });
   }
 
-  insert: function(cb) {
-    orm.insertOne('burgers', cols, vals, function(res) {
-      cb(res);
+  insert: function(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, function(result) {
+      cb(result);
     });
   }
 
-  update: function(cb) {
-    orm.updateOne('burgers', objColVals, condition, function(res) {
-      cb(res)
+  update: function(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, function(result) {
+      cb(result)
     });
   }
 }
