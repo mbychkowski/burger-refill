e26819804873ff637b01e45f5e5253b63d638633
michael bychkowski <mbychkowski@gmail.com>
2018-03-09 23:13:16 -0500

    add POST ajax call to add burger

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('this is working');
+$(function() {
+
+  $('#order-burger').on('click', function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $('#burger-input').val().trim(),
+      devoured: false
+    };
+
+    // Send the POST request.
+    $.ajax('/api/burger', {
+      type: 'POST',
+      data: newBurger
+    }).then(
+      function() {
+        console.log('created new plan');
+
+        location.reload();
+      }
+    );
+  });
+
+});
