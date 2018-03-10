9b74a71f43692d76cfba3a018b91f587a42eaa84
michael bychkowski <mbychkowski@gmail.com>
2018-03-09 21:58:26 -0500

    sketch out handlebars template

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
-var mysql      = require('mysql');
+var mysql = require('mysql');
+
 var connection = mysql.createConnection({
 
   port     : 3306,
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var router     = require('./controllers/burgers_controller.js');
-var exphbs     = require("express-handlebars");
+var exphbs     = require('express-handlebars');
 var bodyParser = require('body-parser');
 var express    = require('express');
 
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -15,10 +15,10 @@ app.use(bodyParser.json())
 
 app.use(router);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
 app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+  console.log('App now listening at localhost:' + PORT);
 });
